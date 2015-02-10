555f9551ddaeac04ef00601256adfd74554a0e8c
Michael Head <michaelehead@gmail.com>
2015-02-10 10:12:10 -0500

    Added production DB connection info.

--- app.rb
+++ app.rb
@@ -15,7 +15,13 @@ set :bind, "0.0.0.0"
 configure :production do
   # Do not serve static assets with sinatra in production
   set :static, false
-  DataMapper.setup(:default, "postgres://postgres:#{ENV["POSTGRES_PW"]}@localhost/changeblindness")
+  
+  db_user = ENV["OPENSHIFT_POSTGRESQL_DB_USERNAME"]
+  db_password = ENV["OPENSHIFT_POSTGRESQL_DB_PASSWORD"]
+  db_host = ENV["OPENSHIFT_POSTGRESQL_DB_HOST"]
+  db_port = ENV["OPENSHIFT_POSTGRESQL_DB_PORT"]
+  db_name = ENV["OPENSHIFT_GEAR_NAME"]
+  DataMapper.setup(:default, "postgresql://#{db_user}:#{db_password}@#{db_host}:#{db_port}/#{db_name}")
 end
 
 configure :development do
